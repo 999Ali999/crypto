bad4bfd3d2011917bfe6c7ad17ffceef5ad246ff
999Ali999 <999alishafiei999@gmail.com>
2025-01-12 13:02:05 +0100

    Add CardComponent and UI card components; update App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import { CardComponent } from "./components/card";
 import { Header } from "./components/header";
 
 function App() {
   return (
     <>
       <Header />
+      <main className="py-24 px-8">
+        <CardComponent />
+      </main>
     </>
   );
 }
